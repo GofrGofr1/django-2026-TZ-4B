7a9d15101dc8ad3aba40e26f7b9590caf14d7ed3
Tomasz Zielinski <tomek07zielinski@wp.pl>
2025-11-05 07:57:26 +0100

    modified:   mysite/polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,6 +7,7 @@ from django.views import generic
 from .models import Choice, Question
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.db.models import F
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
